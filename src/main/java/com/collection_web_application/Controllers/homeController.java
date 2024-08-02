5f5e8df939c072e5feb709fe357b64253756f673
Clock <mdsajjadhossain614@gmail.com>
2024-08-02 23:40:48 +0600

    second commit

--- src/main/java/com/collection_web_application/Controllers/homeController.java
+++ src/main/java/com/collection_web_application/Controllers/homeController.java
@@ -1,4 +1,4 @@
-package com.collection_web_application.Controller;
+package com.collection_web_application.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
