d9616a8607fb2b1eca1cc71ff748e32a0c16a283
Clock <mdsajjadhossain614@gmail.com>
2024-08-02 23:28:22 +0600

    first initialize

---
+++ src/test/java/com/collection_web_application/CollectionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection_web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
