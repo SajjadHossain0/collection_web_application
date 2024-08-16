5319e33c99991ac1fae30c7c9221ce9322f40e20
Clock <mdsajjadhossain614@gmail.com>
2024-08-16 23:28:59 +0600

    new commit

---
+++ src/main/java/com/collection_web_application/Configurations/languageConfig.java
@@ -0,0 +1,62 @@
+package com.collection_web_application.Configurations;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class languageConfig implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8"); // Ensure UTF-8 encoding
+        return messageSource;
+    }
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
+
+
+/*
+messages.properties (English)
+
+navbar.home=Home
+navbar.about=About
+navbar.contact=Contact
+
+messages_bn.properties (Bengali)
+
+navbar.home=বাড়ি
+navbar.about=সম্পর্কে
+navbar.contact=যোগাযোগ
+*/
